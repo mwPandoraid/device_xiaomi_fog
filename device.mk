9a38fa3cb7d3e753165226b16fe24151caf454e1
strwbrry143 <alexfinhart@gmail.com>
2022-08-04 18:43:24 +0800

    fog: Build missing C2 codecs
    * we only need the codecs since were are using Codec2 services: software
    
    to satisfy:
    08-04 18:05:46.501  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.avc.encoder at line 74 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.501  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.h263.encoder at line 80 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.501  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.mpeg4.encoder at line 83 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.501  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.vp8.encoder at line 86 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.501  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.vp9.encoder at line 92 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.501  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.hevc.encoder at line 97 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.503  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.avc.decoder at line 174 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.503  1213  1213 I HidlServiceManagement: Registered vendor.qti.imsrtpservice@3.0::IRTPService/imsrtpservice
    08-04 18:05:46.503  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.hevc.decoder at line 180 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.503  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.mpeg4.decoder at line 187 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.503  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.h263.decoder at line 190 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.503  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.vp9.decoder at line 194 of /vendor/etc/media_codecs_performance_kona.xml
    08-04 18:05:46.503  1200  1200 D MediaCodecsXmlParser: MediaCodec: cannot update non-existing codec: c2.android.vp8.decoder at line 200 of /vendor/etc/media_codecs_performance_kona.xml
    
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -275,6 +275,21 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Media - C2 codecs
+PRODUCT_PACKAGES += \
+    libcodec2_soft_avcdec \
+    libcodec2_soft_avcenc \
+    libcodec2_soft_h263dec \
+    libcodec2_soft_h263enc \
+    libcodec2_soft_mpeg4dec \
+    libcodec2_soft_mpeg4enc \
+    libcodec2_soft_vp8dec \
+    libcodec2_soft_vp8enc \
+    libcodec2_soft_vp9dec \
+    libcodec2_soft_vp9enc \
+    libcodec2_soft_hevcdec \
+    libcodec2_soft_hevcenc
+
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
