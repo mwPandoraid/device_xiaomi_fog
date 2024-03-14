b66901b6a7466a3b1840cccc3de016ce85c03c81
alternoegraha <noegrahachan@gmail.com>
2024-03-14 03:32:30 +0000

    fog: Bring and adapt MIUI Camera
    - clone MIUI cam from gitlab
    - remove duplicate system properties
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 # Clone kernel tree
 git clone --depth=1 https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
-# Clone my fork of hardware/xiaomi
-# git clone https://github.com/alternoegraha/hardware_xiaomi hardware/xiaomi
+# Clone MIUICamera
+git clone --depth=1 https://gitlab.com/alternoegraha/proprietary_vendor_xiaomi_camera vendor/xiaomi/camera
 
 # Kernel fixups
 cd kernel/xiaomi/fog
